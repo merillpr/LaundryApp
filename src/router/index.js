5df2d43e2af408936bf7e3847d248cda6bdb8b30
merillpr <merillpr16@gmail.com>
2024-03-22 23:19:22 +0700

    [+] screen structure

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Splash from "../pages/Splash";
+import Home from "../pages/Home";
+import Account from "../pages/Account";
+import Order from "../pages/Order";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Order"
+        component={Order}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const Router = () => {
+  return (
+    <Stack.Navigator initialRouteName="MainApp">
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
+
+const styles = StyleSheet.create({});
